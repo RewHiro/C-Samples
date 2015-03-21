1f04356bb91e67adf8b9c514e6559fb9ecb8cede
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-03-21 21:33:50 +0900

    [新規]SceneSample

---
+++ SceneSample/src/Scene.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "lib\texture.hpp"
+#include "lib\graph.hpp"
+
+enum Window {
+	WIDTH = 256,
+	HEIGHT = 256
+};
+
+class Scene
+{
+	const Color color = Color(1, 1, 1);
+protected:
+	const Texture bg_texture;
+	const Texture chara_texture;
+	const Texture text_texture;
+public:
+	Scene(
+		std::string bg_texture,
+		std::string chara_texture,
+		std::string text_texture
+		) :
+		bg_texture(Texture(bg_texture)),
+		chara_texture(Texture(chara_texture)),
+		text_texture(Texture(text_texture))
+	{
+	}
+	virtual ~Scene() = default;
+
+	virtual void Draw(){
+		drawTextureBox(-Window::HEIGHT*.5f, -Window::HEIGHT*.5f, bg_texture.width(), bg_texture.height(),
+			0, 0, bg_texture.width(), bg_texture.height(),
+			bg_texture, color);
+
+		drawTextureBox(-Window::HEIGHT*.5f, -Window::HEIGHT*.5f, chara_texture.width(), chara_texture.height(),
+			0, 0, chara_texture.width(), chara_texture.height(),
+			chara_texture, color);
+
+		drawTextureBox(-Window::HEIGHT*.5f, -Window::HEIGHT*.5f, text_texture.width(), text_texture.height(),
+			0, 0, text_texture.width(), text_texture.height(),
+			text_texture, color);
+	}
+};
+
---
+++ SceneSample/src/main.cpp
@@ -0,0 +1,44 @@
+﻿//
+// アプリ雛形
+//
+
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+#include "Scene.h"
+#include <array>
+
+
+enum class SceneType{
+	TITLE,
+	STAGE,
+	RESULT,
+};
+
+// 
+// メインプログラム
+// 
+int main() {
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, true);
+
+  std::array<Scene,3>scenes ={
+	  Scene("res/bg1.png","res/chara1.png","res/text1.png"),
+	  Scene("res/bg2.png", "res/chara2.png", "res/text2.png"),
+	  Scene("res/bg3.png", "res/chara3.png", "res/text3.png")
+  };
+
+
+  int scene_count = 0;
+  while (env.isOpen()) {
+
+	  Scene& scene = scenes.at(scene_count % 3);
+	  if(env.isPushButton(Mouse::LEFT)){
+		  scene_count++;
+	  }
+    env.setupDraw();
+    
+	scene.Draw();
+
+    
+    env.update();
+  }
+}
