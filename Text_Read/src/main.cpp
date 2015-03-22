5ea613592c8f3a6e0de8f54376c1321a576c388d
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-03-22 16:55:20 +0900

    [新規]Text_Read

---
+++ Text_Read/src/main.cpp
@@ -0,0 +1,53 @@
+﻿//
+// アプリ雛形
+//
+
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+#include <fstream>
+
+enum Window {
+  WIDTH  = 512,
+  HEIGHT = 512
+};
+
+class Object{
+	const Texture texture;
+	Color color;
+public:
+
+	Object(std::string res_name):
+		texture(Texture(res_name)),
+		color(Color(1,1,1))
+	{
+	}
+
+	void Draw(){
+		drawTextureBox(0, 0, 128, 128,
+			0, 0, texture.width(), texture.height(),
+			texture, color);
+	}
+};
+
+// 
+// メインプログラム
+// 
+int main() {
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, true);
+  
+  std::fstream load("res/data.txt");
+  std::string res_name;
+
+  load >> res_name;
+
+  Object player(res_name);
+
+
+  while (env.isOpen()) {
+    env.setupDraw();
+    
+	player.Draw();
+    
+    env.update();
+  }
+}
