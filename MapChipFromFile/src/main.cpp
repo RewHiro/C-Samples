69be985517a09175d953e51df0eb1fe96765183e
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-03-22 22:39:43 +0900

    [新規]MapChipFromFile

---
+++ MapChipFromFile/src/main.cpp
@@ -0,0 +1,27 @@
+﻿//
+// アプリ雛形
+//
+
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+
+
+enum Window {
+  WIDTH  = 512,
+  HEIGHT = 256
+};
+
+
+// 
+// メインプログラム
+// 
+int main() {
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, false);
+  
+  while (env.isOpen()) {
+    env.setupDraw();
+    
+    
+    env.update();
+  }
+}
